1bbbdd2488064ce892ca114237974bb18622d683
Andrew Weinstein <andrew.weinstein@invitae.com>
2022-06-12 23:09:59 -0600

    set up group templates

--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.mixins import (LoginRequiredMixin,
+                                        PermissionRequiredMixin)
+from django.urls import reverse
+from django.views import generic as generic_views
+
+from .models import Group, GroupMember
+
+
+class CreateGroupView(LoginRequiredMixin, generic_views.CreateView):
+    fields = ('name', 'description')
+    model = Group
+
+
+class SingleGroupView(generic_views.DetailView):
+    model = Group
+
+
+class GroupListView(generic_views.ListView):
+    model = Group
