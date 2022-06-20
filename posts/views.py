39b49d8a96c4b5c41fc0c9db89a08491fe76a10b
Andrew Weinstein <aweinstein7@gmail.com>
2022-06-19 18:59:03 -0600

    groups and posts migrations and urls

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from braces.views import SelectRelatedMixin
 
-# Create your views here.
+from django.contrib import messages
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import Http404
+from django.urls import reverse_lazy
+from django.shortcuts import render
+from django.views import generic
+
+from . import forms, models
+
+User = get_user_model()
+
+
+class PostList(SelectRelatedMixin, generic.ListView):
+    model = models.Post
+    select_related = ('user', 'group')
+
+
+class UserPosts(generic.ListView):
+    model = models.Post
+    template_name = 'posts/user_post_list.html'
+    post_user = None
+
+    def get_queryset(self):
+        try:
+            username = self.kwargs.get('username')
+            self.post_user = User.objects.prefetch_related('posts').get(username__iexact=username)
+        except User.DoesNotExist:
+            raise Http404
+        else:
+            return self.post_user.posts.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post_user'] = self.post_user
+        return context
+
+
+class PostDetail(SelectRelatedMixin, generic.DetailView):
+    model = models.Post
+    select_related = ('user', 'group')
+
+    def get_queryset(self):
+        username = self.kwargs.get('username')
+        return super().get_queryset().filter(user__username__iexact=username)
+
+
+class CreatePost(LoginRequiredMixin, SelectRelatedMixin, generic.CreateView):
+    model = models.Post
+    fields = ('message', 'group')
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+
+
+class DeletePost(LoginRequiredMixin, SelectRelatedMixin, generic.DeleteView):
+    model = models.Post
+    select_related = ('user', 'group')
+    success_url = reverse_lazy('posts:all')
+
+    def get_queryset(self):
+        super().get_queryset().filter(user_id=self.request.user.id)
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(request, 'Post deleted')
+        return super().delete(request, *args, **kwargs)
